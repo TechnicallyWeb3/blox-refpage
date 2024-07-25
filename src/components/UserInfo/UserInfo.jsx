e37f6823e4c785bd5c674deaecb48ab4ffbf0b16
ChrisHashing <chriswindow466@gmail.com>
2024-07-26 00:26:41 +0530

    ui fixe

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -5,21 +5,26 @@ import refresh from '../../assets/refresh-arrow.png'
 import user from '../../assets/image.png'
 function UserInfo() {
     return (
-        <div className={styles.userInfo}>
-            <div className={styles.userdetails}>
-                <img src={user} alt="" />
-                <p className={styles.name}>Chris Hash</p>
-                <p>@chrishash</p>
-            </div>
-            <div className={styles.referralCode}>
-                <p>Referral Code:</p>
-                <div className={styles.refresh_container}>
-                    <p className={styles.refCode}>CLOUD420</p>
-                    <button className={styles.neumorphicbtn}><img src={refresh} alt="" /></button>
-                    <button className={styles.neumorphicbtn}><img src={copy} alt="" /></button>
+        <div className={styles.userbox}>
 
+            <div className={styles.userInfo}>
+                <div className={styles.userdetails}>
+                    <img src={user} alt="" />
+                    <p className={styles.name}>Chris Hash</p>
+                    <p>@chrishash</p>
+                </div>
+                <div className={styles.referralCode}>
+                    <p>Referral Code:</p>
+                    <div className={styles.refresh_container}>
+                        <p className={styles.refCode}>CLOUD420</p>
+                        <button className={styles.neumorphicbtn}><img src={refresh} alt="" /></button>
+                        <button className={styles.neumorphicbtn}><img src={copy} alt="" /></button>
+
+                    </div>
                 </div>
             </div>
+
+
         </div>
     );
 }
