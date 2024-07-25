fdafad65bb77fed7c89eac11732e6ca488e833a3
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2024-07-24 20:18:11 -0400

    Update UserInfo.jsx

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import styles from './UserInfo.module.css';
+import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 
 function UserInfo() {
-    return (
-        <div className={styles.userInfo}>
-            <div>
-                <p>Chris Hash</p>
-                <p>@chrishash</p>
-            </div>
-            <div className={styles.referralCode}>
-                <p>Referral Code:</p>
-                <div className={styles.refresh_container}>
-                    <p>CLOUD420</p>
-                    <button>Refresh code</button>
-                    <button>Copy code</button>
+  const { isAuthenticated, user, primaryWallet } = useDynamicContext();
 
-                </div>
-            </div>
+  if (!isAuthenticated || !(user?.email || primaryWallet)) {
+    return null; // Return nothing if the user is not authenticated or no email/wallet is present
+  }
+
+  return (
+    <div>
+      <div className={styles.userInfo}>
+        <div>
+          <p className="text-lg font-bold mb-2">Hi! {user?.firstName} {user?.lastName}</p>
+          <p className="text-lg font-bold mb-2">{user?.username}</p>
         </div>
-    );
+      </div>
+
+      <div className={styles.referralCode}>
+        <p>Referral Code:</p>
+        <div className={styles.refresh_container}>
+          <p>CLOUD420</p>
+          <button>Refresh code</button>
+          <button>Copy code</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default UserInfo;
