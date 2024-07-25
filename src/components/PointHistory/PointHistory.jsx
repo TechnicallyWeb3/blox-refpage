1701d10e7ec10550c5ea91d30e7763b35b204b77
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2024-07-25 01:59:47 -0400

    she's not ready for her picture yet, so she applied some makeup

--- src/components/PointHistory/PointHistory.jsx
+++ src/components/PointHistory/PointHistory.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './PointHistory.module.css';
+import chartImage from '../../assets/chart.png'; // Import the image file
 
 function PointHistory() {
     return (
         <div className={styles.pointHistory}>
-            <p style={{ marginBottom: "25px", fontWeight: "700",fontSize:"20px" }}>Point History</p>
-            <div className={styles.chart}>Chart</div>
+            <p style={{ marginBottom: "25px", fontWeight: "700", fontSize: "20px" }}>Point History</p>
+            <div className={styles.chart}>
+                <img src={chartImage} alt="Chart" className={styles.chartImage} />
+                <div className={styles.overlay}>
+                    <p className={styles.overlayText}>NOT ENOUGH DATA</p>
+                </div>
+            </div>
         </div>
     );
 }
