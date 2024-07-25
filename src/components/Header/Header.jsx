1c78829c6aa1eb5d52bf5aa65eaca452c972590e
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2024-07-24 20:24:43 -0400

    she's dynamic & waiting to get spicy

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Header.module.css';
-import {
-    DynamicWidget,
-} from "@dynamic-labs/sdk-react-core";
+import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 
 function Header() {
     return (
         <header className={styles.header}>
-            <div className={styles.logo}>Logo</div>
+            <div className={styles.logo}>
+                <img src="/blox400.jpg" alt="Logo" className={styles.logoImage} />
+            </div>
             <DynamicWidget />
         </header>
     );
