617eac595c89509f245290234f660c5b2ac9daf9
ChrisHashing <chriswindow466@gmail.com>
2024-07-27 02:14:44 +0530

    added new bg

--- src/pages/Home/landing.jsx
+++ src/pages/Home/landing.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import style from "./landing.module.css";
 import Spline from '@splinetool/react-spline';
-import img from '../../assets/reed1.png';
-import img1 from '../../assets/reed-mob1.png';
+import img from '../../assets/BG2.png';
+import img1 from '../../assets/BGMOB1.png';
 
 export default function Landing() {
   const [isMobile, setIsMobile] = useState(false);
