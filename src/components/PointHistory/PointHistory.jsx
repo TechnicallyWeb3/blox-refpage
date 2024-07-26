77b7f616f50f7684f115c565b72331ac097d0665
ChrisHashing <chriswindow466@gmail.com>
2024-07-27 03:29:15 +0530

    fix chart

--- src/components/PointHistory/PointHistory.jsx
+++ src/components/PointHistory/PointHistory.jsx
@@ -7,9 +7,9 @@ function PointHistory() {
         <div className={styles.pointHistory}>
             <p style={{ marginBottom: "25px", fontWeight: "700", fontSize: "20px" }}>Point History</p>
             <div className={styles.chart}>
-                <img src={chartImage} alt="Chart" className={styles.chartImage} />
+                {/* <img src={chartImage} alt="Chart" className={styles.chartImage} /> */}
                 <div className={styles.overlay}>
-                    <p className={styles.overlayText}>NOT ENOUGH DATA</p>
+                    <p className={styles.overlayText}>COMING SOON</p>
                 </div>
             </div>
         </div>
