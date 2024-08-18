2fd383b66b3df635dac63b5efd0fe7e004a56870
technicallyweb3 <mancinoweb3@gmail.com>
2024-08-18 12:59:52 -0600

    added presale link button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,11 +11,19 @@ import logo from '../../assets/BLOXlogo.svg'
 function Header() {
     return (
         <header className={styles.header}>
-            <a href="https://bloxsolutions.app">
-                <button className="appBtn">
-                    Go To App
-                </button>
-            </a>
+            <div style={{ display: 'flex', gap: '10px' }}>
+                <a href="https://bloxsolutions.app">
+                    <button className="appBtn">
+                        Go To App
+                    </button>
+                </a>
+                <a href="https://buy.stripe.com/cN2g20gbXfyZdKE3cc">
+                    <button className="appBtn">
+                        Blox Presale
+                    </button>
+                </a>
+            </div>
+
             <DynamicWidget />
         </header>
     );
